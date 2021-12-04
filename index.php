9d519e7f95eac39b8296026e3d50d9858b42b180
cangussu12 <leo.tywin@gmail.com>
2021-12-03 21:59:26 -0300

    fetchAll

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
-    $user = 'root';
-    $password = '';
+    $dsn = 'mysql:host=localhost;dbname=db_php_com_pdo';
+    $usuario = 'root';
+    $senha = '';
 
-    $conexao = new PDO($dsn, $user, $password);
+    try{
+        $conexao = new PDO($dsn, $usuario , $senha);
+        
+        $query = '
+            select * from tb_usuarios
+        ';
 
-?>
+       $stmt = $conexao->query($query);
+       $lista = $stmt->fetchAll();
+       echo '<pre>';
+       print_r($lista);
+       echo '</pre>';
+
+        echo $lista[1]['email'];
+
+    } catch(PDOException $e) {
+        // echo '<pre>';
+        //     print_r($e);
+        // echo '</pre>';
+
+        echo 'Erro: '. $e->getCode(). ' Mensagem: '. $e->getMessage();
+        //registrar o erro de alguma forma.
+    }
