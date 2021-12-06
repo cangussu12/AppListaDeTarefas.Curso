c26c60d5f111a28b2a9abd3fbb096ecdc97aa58f
cangussu12 <leo.tywin@gmail.com>
2021-12-05 22:00:21 -0300

    sql inject

--- index.php
+++ index.php
@@ -1,29 +1,52 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=db_php_com_pdo';
-    $usuario = 'root';
-    $senha = '';
+    if(!empty($_POST['usuario']) && !empty($_POST['senha'])) {
 
-    try{
-        $conexao = new PDO($dsn, $usuario , $senha);
-        
-        $query = '
-            select * from tb_usuarios where id = 2 
-        ';
 
-       $stmt = $conexao->query($query);
-       $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
-       echo '<pre>';
-       print_r($usuario);
-       echo '</pre>';
+        $dsn = 'mysql:host=localhost;dbname=db_php_com_pdo';
+        $usuario = 'root';
+        $senha = '';
 
-        echo $usuario['nome'];
+        try{
+            $conexao = new PDO($dsn, $usuario , $senha);
 
-    } catch(PDOException $e) {
-        // echo '<pre>';
-        //     print_r($e);
-        // echo '</pre>';
+            $query = "select * from tb_usuarios where ";
+            $query .= " email = '{$_POST['usuario']}' ";
+            $query .= " AND senha = '{$_POST['senha']}' ";
+            
+            echo $query;
 
-        echo 'Erro: '. $e->getCode(). ' Mensagem: '. $e->getMessage();
-        //registrar o erro de alguma forma.
+            $stmt = $conexao->query($query);
+            $usuario = $stmt->fetchAll();
+            echo '<hr/>';
+
+            echo '<pre>';
+            //print_r($usuario);
+            echo '</pre>';
+
+        } catch(PDOException $e) {
+            echo 'Erro: '. $e->getCode(). ' Mensagem: '. $e->getMessage();
+            //registrar o erro de alguma forma.
+        }
     }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form method="post" action="index.php">
+        <input type="text" placeholder="usuario" name="usuario"/>
+        <br />
+        <input type="password" placeholder="senha" name="senha"/>
+        <br />
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
